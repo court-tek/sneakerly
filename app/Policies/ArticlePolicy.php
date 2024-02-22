5a0d355ff10e05594886b22136d7567f24d84022
court-tek <dishoughng@gmail.com>
2024-02-22 11:50:10 -0500

    created an articles policy

---
+++ app/Policies/ArticlePolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ArticlePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->id === 1 || $user->id === 2;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Article $article): bool
+    {
+        return $user->id === 1 || $user->id === 2 || (auth()->check() && $article->user_id === auth()->id);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->id == 2 || $user->id === 3;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Article $article): bool
+    {
+        return $user->id === 2 || (auth()->check() && $article->user_id === auth()->id);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Article $article): bool
+    {
+        return $user->id === 2 || (auth()->check() && $article->user_id === auth()->id);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    // public function restore(User $user, Article $article): bool
+    // {
+    //     //
+    // }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    // public function forceDelete(User $user, Article $article): bool
+    // {
+    //     //
+    // }
+}
